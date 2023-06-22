837dd63e8b1f49162fb738393d4db7180cb92a06
anthonySnaps <anthony.lee@snaps.com>
2023-06-22 22:20:24 +0900

    webpack4: webpack devServer 세팅 및 proxy 설정

---
+++ webpack/webpack4-devServer/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+module.exports = {
+  mode,
+  entry: {
+    main: "./src/app.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+    assetModuleFilename: 'images/[hash][ext][query]'
+  },
+  devServer: {
+    client: {
+      overlay: true,
+    },
+    devMiddleware: {
+      stats: "errors-only",
+    },
+    proxy: {
+      '/api': 'http://localhost:8081',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader" // 바벨 로더를 추가한다
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+    }),
+    new webpack.DefinePlugin({}),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+            collapseWhitespace: true, // 빈칸 제거
+            removeComments: true // 주석 제거
+          }
+          : false,
+      hash: process.env.NODE_ENV === "production"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: `[name].css` })
+  ]
+};
